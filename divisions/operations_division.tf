4b283bf2f2531cfc97d0da6894e82367197cb229
VEDULA <mallikarjuna-rao.vedula@capgemini.com>
2026-05-23 12:27:12 +0530

    added new division operations and assigned to cicd_test queue

---
+++ divisions/operations_division.tf
@@ -0,0 +1,4 @@
+resource "genesyscloud_auth_division" "operations_division" {
+  name        = "Operations-Division"
+  description = "Operations Division for operations related to day-to-day operations"
+}
--- divisions/outputs.tf
+++ divisions/outputs.tf
@@ -4,5 +4,6 @@ output "division_ids" {
   value = {
     hr_division = genesyscloud_auth_division.hr_division.id
     sales_division = genesyscloud_auth_division.sales_division.id
+    operations_division = genesyscloud_auth_division.operations_division.id
   }
 }
