5a055d48204414fc9266a4153ccfee862e14fab8
VEDULA <mallikarjuna-rao.vedula@capgemini.com>
2026-05-26 16:34:28 +0530

    removed comments and trying to fix bulls eye rings

--- queues/ExampleDemoTest.tf
+++ queues/ExampleDemoTest.tf
@@ -2,39 +2,26 @@ resource "genesyscloud_routing_queue" "ExampleDemoTest" {
   name        = "ExampleDemoTest"
   description = "Example Queue for Demo"
 
-  #############################################
-  # DIVISION
-  #############################################
+  
   division_id = var.division_ids["hr_division"]
 
-  #############################################
-  # MEMBERS
-  #############################################
   members {
     user_id  = var.user_ids["Harry_Porter"]
     ring_num = 1
   }
 
-  #############################################
-  #  AFTER CALL WORK
-  #############################################
   acw_wrapup_prompt = "MANDATORY_TIMEOUT"
   acw_timeout_ms    = 120000
 
-  #############################################
-  #  AUTO ANSWER
-  #############################################
+ 
   auto_answer_only = true
 
-  #############################################
-  # WRAP-UP CODES
-  #############################################
+  
   wrapup_codes = [
     var.wrapup_code_ids["billing_success"],
     var.wrapup_code_ids["issue_resolved"]
   ]
 
-  #############################################
   # ✅ BULLSEYE ROUTING
   #Ring 1: no relaxation of skills
   
