f9a8281324e1d517f27c02ddd14e8f90b7c93312
Swetha <addanki.swetha@capgemini.com>
2026-06-02 14:25:46 +0530

    adding ring with 2 empty skill

--- queues/ExampleDemoTest_v2.tf
+++ queues/ExampleDemoTest_v2.tf
@@ -26,14 +26,12 @@ resource "genesyscloud_routing_queue" "ExampleDemoTest_v2" {
   bullseye_rings {
     expansion_timeout_seconds = 20
     skills_to_remove = [
-      var.skill_ids[]
     ]
   }
 
   bullseye_rings {
     expansion_timeout_seconds = 30
     skills_to_remove = [
-      var.skill_ids[]
     ]
   }
 
