c50d5ada720e8c03c522c804b09bb8646efb670d
VEDULA <mallikarjuna-rao.vedula@capgemini.com>
2026-06-05 13:11:17 +0530

    trying to get the script resource

--- scripts/outputs.tf
+++ scripts/outputs.tf
@@ -2,6 +2,6 @@ output "script_ids" {
   description = "Map of all created script references to their Genesys Cloud IDs"
 
   value = {
-    available_agent_on_queue = data.genesyscloud_script.AvailableAgentOnQueue.id
+    AvailableAgentOnQueue = data.genesyscloud_script.AvailableAgentOnQueue.id
   }
 }
