a23ad206670442c5dafa3ebc9db98bbca69585ce
VEDULA <mallikarjuna-rao.vedula@capgemini.com>
2026-06-16 11:55:12 +0530

    11:55

--- architect_flows/main.tf
+++ architect_flows/main.tf
@@ -3,12 +3,13 @@ resource "genesyscloud_flow" "basic_cicd_flow" {
   filepath = "${path.module}/inbound_flows/Basic_CICD_Flow.yaml"
 }
 
-resource "genesyscloud_flow" "Inqueue_CICD" {
-  
-  filepath = "${path.module}/inqueue_flows/Inqueue_CICD_v1-0.yaml"
-}
 
 resource "genesyscloud_flow" "TestInQueueFlow" {
   
   filepath = "${path.module}/inqueue_flows/TestInQueueFlow.yaml"
+}
+
+resource "genesyscloud_flow" "Inqueue_CICD_v1-0" {
+  
+  filepath = "${path.module}/inqueue_flows/Inqueue_CICD_v1-0.yaml"
 }
