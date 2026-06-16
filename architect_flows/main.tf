f872fa7ff207a6120a867ac67cc07f539bd2d432
VEDULA <mallikarjuna-rao.vedula@capgemini.com>
2026-06-16 17:19:16 +0530

    inbound main

--- architect_flows/main.tf
+++ architect_flows/main.tf
@@ -2,6 +2,10 @@ module "inqueue_flows" {
   source = "./inqueue_flows"
 }
 
+module "architect_flows" {
+  source = "./architect_flows"
+}
+
 output "inqueue_flow_ids" {
   value = module.inqueue_flows.inqueue_flow_ids
 }
